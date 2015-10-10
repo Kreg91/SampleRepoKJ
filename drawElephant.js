45dda3edd7b5754cc111898e07adb1f70f5b9f57
Krzysztof J <fxjkqwer@gmail.com>
2015-10-10 20:41:32 +0200

    comment KJ proba

--- drawElephant.js
+++ drawElephant.js
@@ -5,7 +5,10 @@ function drawElephant(){
     var canvas = document.getElementById('FirstExample3');
     var ctx=canvas.getContext('2d');
     ctx.fillStyle='rgba(255,0,0,1.0)';
-    ctx.fillRect(2,2,300,300);
+    ctx.fillRect(200,800,50,250);
+    ctx.fillRect(260,750,50,250);
+    ctx.fillRect(500,800,50,250);
+    ctx.fillRect(560,750,50,250);
     // fillRect(x,y,w,h) dane kwadratu
     //napisz kod dla trójk¹ta masz funkcje w zeszycie w notatkach
     ctx.beginPath();
@@ -16,4 +19,17 @@ function drawElephant(){
     ctx.lineTo(340,250);
     ctx.closePath();
     ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(180,750);
+    ctx.lineTo(700,730);
+    ctx.lineTo(650,660);
+    ctx.lineTo(190,700);
+    ctx.lineTo(180,750);
+
+    ctx.closePath();
+    ctx.fill();
+    ctx.lineWidth=5;
+    ctx.strokeStyle="black";
+    ctx.stroke();
 }
